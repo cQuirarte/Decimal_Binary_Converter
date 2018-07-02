6d4e58eba9dbd3f4715f7d11349cefe2a7b03a98
Carlos Quirarte <cQuirarteJr@gmail.com>
2018-07-01 18:29:41 -0700

    Fixed bugs. Need to clear lists in menu 2.

--- DecimalBinaryConverter.py
+++ DecimalBinaryConverter.py
@@ -4,6 +4,11 @@
 # convert single decimal values or an entire IP address into binary.
 
 
+class style:
+   BOLD = '\033[1m'
+   END = '\033[0m'
+
+
 def convert(value1):
     list1 = list()  # create an empty list
     list2 = list()
@@ -11,7 +16,7 @@ def convert(value1):
     t = 0
     val1 = value1 # copy value1 content to val1
 
-    print ("in Convert function")
+    print ("in Convert function")   ### testing purposes
 
     # Convert val1 to str if int so we can iterate over it
     try:
@@ -21,7 +26,7 @@ def convert(value1):
     except:
         pass
 
-    # Code to convert a decimal IP address to binary and store as list
+    # Algorithm to convert a decimal IP address to binary and store as list
     for v in range(0, len(val1)):
 
         # block of code for my testing purposes
@@ -63,8 +68,6 @@ def convert(value1):
                 break
             elif (len(list1) == 8 and len(val1) == 1):
                 list2 += list1
-                # displayBinary(list1) # Call on function to display binary conversion  # may delete
-
         except:
             if (val1[v] == "."):
                 print ("test 5533")
@@ -75,6 +78,7 @@ def convert(value1):
                 print (list2)
                 print ("test 2999")
                 list1 = list()  # clear the list
+
         print ("test 998")
         t += 1
         print ("iteration ### ", t)
@@ -82,33 +86,11 @@ def convert(value1):
             break
     print ("test 9333")
 
+    # Display in binary after the converted
     displayBinary(list2)
 
-    #### keep, might need it
-    # try:
-    #     # if val1 is a single int value
-    #     while (val1 >= 1):
-    #         if (val1 % 2 == 0):
-    #             list1.append(0)
-    #         else:
-    #             list1.append(1)
-    #         val1 = val1 / 2
-    #         val1 = int(val1)
-    #     # Ensures that there are 8 total bits
-    #     while (len(list1) < 8):
-    #         list1.append(0)
-    #     displayBinary(list1) # Call on function to display binary conversion
-    # except:
-    #     ### finish writing code
-    #     # continue
-    #     for v in val1:
-    #         try:
-    #             continue
-    #         except:
-    #             continue
 
-
-# Prints the values in reversed order, as it should be
+# Prints the values of the array without space
 def displayBinary(fullList):
     print ("Binary Value: " , end='')
     for item in (fullList):
@@ -116,15 +98,14 @@ def displayBinary(fullList):
     print ("\n")
 
 
-# Function to check if between 0 to 255
+# Function to check if user's value is between 0 to 255
 def betweenPar(usrNum):
     if (usrNum >= 0 and usrNum <= 255):
-        print ("test 56")
+        print ("test 56")   # testing purposes
         return True
-    elif (usrNum < 0 or usrNum > 255):
+    elif (usrNum < 0 or usrNum > 255):  # testing purposes
         print ("test 57")
         return False
-
 
 
 # Function to ensure user enters the appropriate responses i.e. "Please enter numbers not letters!"
@@ -141,9 +122,9 @@ def checkUserInput(data):
                 if (betweenPar(finalData)):
                     iter2 = False
                 else:
-                    print ("Please enter a value from 0 to 255")
+                    print ("\n! Please enter a value from 0 to 255")
             except:
-                print ("Please enter a number! 3")
+                print ("\n! Please enter a value from 0 to 255")
     # Convert IP option
     elif (data == 2):
         ipAddress = list()
@@ -172,7 +153,7 @@ def checkUserInput(data):
                         tempList.append(str(n))
                         iter3 += 1 # testing purposes
                         print ("Iteration # ", iter3)
-                        print ("in try")    ### it stops here because for statement reaches 7
+                        print ("test 955")    ### it stops here because for statement reaches 7
                     except:
                         if (iter3 != 0 and (n == " " or n == ".")):
                             iter3 += 1 # testing purposes
@@ -199,43 +180,27 @@ def checkUserInput(data):
                                         print("Iteration # ", iter3, "Final stage")
                                         finalData = ipAddress
                                         again2 = False
-
-
                             else:
-                                print ("Please follow guidelines when entering the IP address!")
-                                print ("Should prompt user to continue. Avoid entering loop...'Separate each octet by space...")
+                                print ("\n! Please follow guidelines when entering the IP address")
+                                print ("! Enter IP address in the format example", style.BOLD + "255.255.255.255" + style.END)
                                 break
                         else:
-                            print ("\nPlease follow guidelines when entering the IP address!")
+                            print ("\n! Please follow guidelines when entering the IP address")
                             break
-                    # write some code here, maybe just 'continue' or delete line after you know that "break"
-                    # allows for the interpreter to flow through here.
-                    print ("Check 2")    ###### delete after taken care of
-
-                ### This code belongs within the for loop to complete the IP address
-                # if (len(ipAddress == 6)):         !!!!
-                #     ipAddress.append(octet)
-                #     print ("Iteration # ", iter3, "Final stage")
-                #     convert(ipAddress)
-
-                # print ("Convert another IP address? ")
-                # if (again1 == "yes"):
-                #     again3 = checkUserInput(3)
-                # elif (again1 == "no"):
-                #     again2 = False
+                    print ("Check 2")    ###### testing purposes
         except:
-            print("Please enter a valid IP address!")
+            print("! fail 511")
     # Check that the user response is either 'yes' or 'no'
     elif (data == 3):
         while (iter2):
-            finalData = input()
+            finalData = input()         ##### NEEDS error control !! FIX THIS!
             # If answer is either yes or no then stop loop otherwise keep asking until accepted answer
             if (finalData == "yes" or finalData == "Yes" or finalData == "YES"):
                 iter2 = False
             elif (finalData == "no" or finalData == "No" or finalData == "NO"):
                 iter2 = False
             else:
-                print ("Please respond by either 'yes' or 'no'\n")
+                print ("\n! Please respond either 'yes' or 'no'\n")
     return finalData
 
 
@@ -289,8 +254,8 @@ if __name__ == "__main__":
                 iter = False    # end loop
                 runProg = False # end program
             else:
-                print ("Please enter a valid choice")
+                print ("\n! Please enter a valid choice")
         except:
-            print ("Please enter a number option! 2")
+            print ("\n! Please enter a number option 2")
 
 print ("-- Program ended --")
